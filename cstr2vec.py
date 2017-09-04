98459605082f76fd5be464251382802b953da8ee
liangzhicheng <liangzhicheng@meizu.com>
2017-09-04 18:35:05 +0800

    123

--- cstr2vec.py
+++ cstr2vec.py
@@ -1,8 +1,10 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 import string
 import numpy as np
 
+
 bit = 16
+
 
 def cstr2vec(aString):
     '''
@@ -13,7 +15,7 @@ def cstr2vec(aString):
     else:
         strBinarySys = bin(ord(aString.decode("utf8")))
     strBinarySys = strBinarySys[2:len(strBinarySys)]
-    strBinarySys = (len(strBinarySys)==15)*"0"+strBinarySys
+    strBinarySys = (len(strBinarySys) == 15) * "0" + strBinarySys
     vec = list(strBinarySys)
     vec = np.array(map(eval, vec))
     return vec
@@ -33,19 +35,21 @@ def centence2array(string):
         array[i, :] = cstr2vec(word)
 
     return array
-    
+
+
 def array2centence(array):
     string_UTF8 = u""
     for row in array:
         vec = map(int, list(row))
-        string_UTF8 = string_UTF8 + str(hex(eval('0b'+"".join(map(str, vec))))).replace('0x','\u')
-    #print(eval('u"'+string_UTF8+'"'))
-    return eval('u"'+string_UTF8+'"')
+        string_UTF8 = string_UTF8 + str(hex(eval('0b' + "".join(map(str, vec))))).replace('0x', '\\u')
+    # print(eval('u"'+string_UTF8+'"'))
+    return eval('u"' + string_UTF8 + '"')
+
 
 if __name__ == "__main__":
-    centence = "哈哈呵呵".decode("utf8")
-    print(centence,centence)
-    array = centence2array(centence)
-    print(array)
-    centence1 = array2centence(array)
-    print(centence1)
+    centence = "哈哈呵呵".encode('utf-8')
+    print(centence, centence)
+    # array = centence2array(centence)
+    # print(array)
+    # centence1 = array2centence(array)
+    # print(centence1)
